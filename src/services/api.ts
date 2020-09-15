d27ba4d127e5e728b00c707d7d7af38f95634844
Marcelo <marcelofscarpim@gmail.com>
2020-09-15 20:56:15 -0300

    Ajustes para subir na loja

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.50:3333',
+  // baseURL: 'http://192.168.0.50:3333',
+  baseURL: 'https://gobarberdc.life',
 });
 
 export default api;
